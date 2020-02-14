ebaa67a0ead1de2912048b72ea461a258685831a
stoictrader <28746824+stoictrader@users.noreply.github.com>
2020-02-14 15:42:35 +0530

    Create example.py

---
+++ example.py
@@ -0,0 +1,7 @@
+from def_expiry import expiry
+#You need to provide an input year in an integer format. You will get a list of expiration dates in list format for that particular year
+
+year = 2019
+expiry_list = expiry(year)
+#To get the expiry date of first month of the year 
+exp_month1 = expiry_list[0]
